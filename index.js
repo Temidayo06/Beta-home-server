9b8b3e8c4f4397f0c27657796ba3ab27170828bf
Temidayo06 <akintomidedayo@gmail.com>
2024-01-29 12:30:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000
+const cors = require('cors')
+const mongoose = require('mongoose')
+const profileRouter = require("./routes/profileRouter")
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//route
+app.get("/", (req, res) => {
+    res.status(200).send("BETA HOME SERVER")
+})
+app.use("/api/v1", profileRouter)
+//error route
+app.use((req, res) => {
+    res.status(404).send("resourse not found")
+})
+
+//db connection
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, { dbName: 'betahome' })
+        app.listen(PORT, () => {
+            console.log(`server running on port : ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer()
